95d639bff648638088330d9783097082a03c991d
KepinNiBoss <punya.kepin@gmail.com>
2022-09-03 23:56:28 +0800

    add intro effect

--- script.js
+++ script.js
@@ -7,3 +7,30 @@ window.addEventListener("load", () => {
     document.body.removeChild("loader");
   });
 });
+
+let intro = document.querySelector(".intro");
+let logo = document.querySelector(".logo-header");
+let logoSpan = document.querySelectorAll(".logo-span");
+
+window.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
+    logoSpan.forEach((span, id) => {
+      setTimeout(() => {
+        span.classList.add("active");
+      }, (id + 1) * 100);
+    });
+
+    setTimeout(() => {
+      logoSpan.forEach((span, id) => {
+        setTimeout(() => {
+          span.classList.remove("active");
+          span.classList.add("fade");
+        }, (id + 1) * 50);
+      });
+    }, 2000);
+
+    setTimeout(() => {
+      intro.style.top = "-100vh";
+    }, 2300);
+  });
+});
