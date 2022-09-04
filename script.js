1ae96d1fd15b512cb28482a3691df2540d1ddb7a
KepinNiBoss <punya.kepin@gmail.com>
2022-09-04 11:34:55 +0800

    add some text in loading effect, and fix navbar bug

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ window.addEventListener("load", () => {
 
 let intro = document.querySelector(".intro");
 let logo = document.querySelector(".logo-header");
+let logo2 = document.querySelector(".logo-second");
+let logo3 = document.querySelector(".logo-third");
 let logoSpan = document.querySelectorAll(".logo-span");
 
 window.addEventListener("DOMContentLoaded", () => {
@@ -27,11 +29,11 @@ window.addEventListener("DOMContentLoaded", () => {
           span.classList.add("fade");
         }, (id + 1) * 50);
       });
-    }, 100); //set 3000
+    }, 4500); //set 3000
 
     setTimeout(() => {
       intro.style.top = "-100vh";
-    }, 100); //set 3500
+    }, 6000); //set 3500
   });
 });
 
@@ -50,3 +52,34 @@ toggle.addEventListener("click", function () {
     body.style.transition = "2s";
   }
 });
+
+setInterval(showTime, 1000); // takes time as 1 s and execute the function for every second.
+
+function showTime() {
+  let today = new Date();
+  let hours = today.getHours();
+  let minutes = today.getMinutes();
+  let seconds = today.getSeconds();
+  let period = "AM";
+
+  // Set the clock in 12 hr format
+  // If hours is equal to 0 at midnight, we set to 12, and if greater than 12, we will
+  //subtract it from 12.
+  if (hours == "0") {
+    hours = 12;
+  }
+  if (hours > 12) {
+    hours -= 12;
+    period = "PM";
+  }
+
+  // when the hrs is less than 10, we are concantenate with 0, otherwise leave it hrs.
+  hours = hours < 10 ? `0${hours}` : hours;
+  minutes = minutes < 10 ? `0${minutes}` : minutes;
+  seconds = seconds < 10 ? `0${seconds}` : seconds;
+
+  // displayin to UI
+  const time = `${hours} : ${minutes} : ${seconds}  ${period} `;
+  document.getElementById("clock").innerHTML = time;
+}
+showTime();
